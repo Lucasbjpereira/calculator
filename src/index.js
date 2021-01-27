eb3ed7383bb3031bcb951d9d04252115b62503b4
Lucas Pereira <lucaspereira@backsite.com.br>
2021-01-26 22:44:46 -0300

    ajuste de estruturação de pastas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Calculator from './components/calculator';
 
 ReactDOM.render(
     <Calculator/>,
   document.getElementById('root')
 );
-
-reportWebVitals();
