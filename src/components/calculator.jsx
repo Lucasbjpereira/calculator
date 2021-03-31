8209e5e517b64d14e83bc294501d77e8db771ae1
Lucas Pereira <lucasbarbosaj.pereira@gmail.com>
2021-03-31 00:34:35 -0300

    Adicionando eventos de teclas na calculadora

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -12,6 +12,7 @@ class Calculator extends React.Component {
         this.clear = this.clear.bind(this);
         this.operator = this.operator.bind(this);
         this.equal = this.equal.bind(this);
+        this.delete = this.delete.bind(this);
         this.operationContent = [];
         this.number = "";
     }
@@ -23,38 +24,69 @@ class Calculator extends React.Component {
         this.number = "";
     }
 
-    operation(e) {
-        this.number += parseInt(e.target.textContent);
+    delete(e) {
+        let teste = document.querySelector('.operation p').textContent.lastIndexOf(" ");
+    }
+
+
+    operation(e, key) {
+        let number = key;
+
+        if (e) {
+            number = e.target.textContent
+        }
+
+        this.number += parseInt(number);
         document.querySelector('.operation p').textContent = this.number;
     }
 
-    equal(e) {
-        this.operator(e);
-        console.log(this.operationContent);
+    equal(e, key) {
+        if (e) {
+            this.operator(e);
+        } else {
+            this.operator(null, key);
+        }
     }
 
-    operator(e) {
+    operator(e, key) {
         if (this.number != "") {
             this.operationContent.push(parseInt(this.number));
         }
 
-        if(this.operationContent[this.operationContent.length - 1] == "=") {
-            this.operationContent.pop();
-            console.log('teste', this.operationContent);
+        let number = key;
+
+        if (e) {
+            number = e.target.textContent
         }
 
+        if (this.operationContent[this.operationContent.length - 1] == "=" || this.operationContent[this.operationContent.length - 1] == "Enter") {
+            this.operationContent.pop();
+        }
+
+        console.log('oi');
         if (!isNaN(this.operationContent[this.operationContent.length - 1])) {
             if (this.operationContent.length == 3) {
                 this.operationContent = [eval(this.operationContent.join(' '))];
                 document.querySelector('.containResult h2').innerHTML = this.operationContent;
             }
 
-            this.operationContent.push(e.target.textContent);
+            this.operationContent.push(number);
             this.number = "";
         }
+
     }
 
     render() {
+        window.addEventListener('keydown', (e) => {
+            if (!isNaN(e.key)) {
+                this.operation(null, e.key);
+            } else if (e.key == '-' || e.key == '*' || e.key == '/' || e.key == '+' || e.key == '%') {
+                this.operator(null, e.key);
+            } else if (e.key == '=' || e.key == 'Enter') {
+                this.equal(null, e.key);
+            }
+        });
+
         return <>
             <section className="display">
                 <div className="operation">
@@ -70,7 +102,7 @@ class Calculator extends React.Component {
                     <Button click={this.clear} class="button blue" value="C" />
                     <Button click={this.operator} class="button blue" value="/" />
                     <Button click={this.operator} class="button blue" value="*" />
-                    <Button class="button blue" value={<FontAwesomeIcon icon={faBackspace} />} />
+                    <Button click={this.delete} class="button blue" value={<FontAwesomeIcon icon={faBackspace} />} />
                 </div>
                 <div className="column">
                     <Button click={this.operation} class="button" value="7" />
